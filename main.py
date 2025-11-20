38c62f9f9d013c982ae558d9717f6ac2d65432ec
savasdan <githubsavas@gmail.com>
2025-11-20 12:27:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ guessed_state = []
 while len(guessed_state) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_state)}/50 the State", prompt="What's another state's name?").title()
     if answer_state == "Exit":
-        missing_states = []     #L17-18-19. You can write missing_states = [state for state in all_states if state not in guessed_states]
+        missing_states = []
         for state in all_states:
             if state not in guessed_state:
                 missing_states.append(state)
@@ -27,7 +27,7 @@ while len(guessed_state) < 50:
         t = turtle.Turtle()
         t.hideturtle()
         t.penup()
-        state_data = data[data.state == answer_state] # This returns True or False. Variable 'state_data' returns 1 row if True !
+        state_data = data[data.state == answer_state] 
         t.goto(state_data.x.item(), state_data.y.item())
         t.write(answer_state)
 
